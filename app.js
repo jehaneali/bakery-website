8910f14b6d82100d166ba0e593fdb5caca129c94
adimohekar <mohekar.a@husky.neu.edu>
2021-01-06 23:38:51 -0600

    Set up server with HTML!!

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const http = require('http');
+var express = require("express");
+var fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  fs.readFile("helloWorld.html", function(error, data) {
+    if(error) {
+      res.writeHead(404)
+      res.write('Error: File Not Found')
+    }
+    else {
+        res.write(data)
+    }    
+    res.end()
+  })
+  // res.write(__dirname+"/helloWorld.html");
+  // // res.setHeader('Content-Type', 'text/plain');
+  // res.end();
 });
 
 server.listen(port, hostname, () => {
